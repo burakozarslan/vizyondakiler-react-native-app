473d9868195f327cfca2863d4a52114d4fb9c53a
burakozarslan <burakozarslan@icloud.com>
2021-08-10 22:57:59 +0100

    Create screen name constants

---
+++ constants/screenConstants.ts
@@ -0,0 +1,4 @@
+export const CITIES_SCREEN = "CitiesScreen";
+export const THEATRES_SCREEN = "TheatresScreen";
+export const MOVIES_SCREEN = "MoviesScreen";
+export const MOVIE_DETAILS_SCREEN = "MovieDetailsScreen";
