3ec14e6c6bff991ae92fab3dd7dc14de86e1a585
burakozarslan <burakozarslan@icloud.com>
2021-08-11 13:08:19 +0100

    Create theatre data type

---
+++ types/dataTypes.ts
@@ -0,0 +1,14 @@
+export type TTheatre = {
+  title: string;
+  address: string;
+  tel: string;
+  features: {
+    three_d: boolean;
+    aircond: boolean;
+    restaurant: boolean;
+    dolby: boolean;
+    phone_res: boolean;
+    parking: boolean;
+  };
+  url: string;
+};
